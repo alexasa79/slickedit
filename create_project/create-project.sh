86fd1557e63f0ccc6739b100d2ad8a899be38fa1
Alexander Sandler <alexander.sandler@gmail.com>
2015-12-08 12:12:22 -0500

    Added option for specifying directory for the project using SLICKEDITPROJ envirnonment variable

--- create_project/create-project.sh
+++ create_project/create-project.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
 
-if [ "$1" == "-h" -o $# -gt 1 ]; then
+if [ "$1" == "-h" -o "$1" == "--help" -o $# -gt 1 ]; then
 	echo ""
 	echo "usage: create-project [project name]"
-	echo "       - will use current directory name as project name unless project name is specified"
+	echo "       - Will create SlickEdit project either in current directory or in directory"
+	echo "         specified by SLICKEDITPROJ environment variable. Project name will be either"
+	echo "         name of current directory or as specified in second argument."
 	echo ""
 	exit 0
 fi
 
 if [ $# -eq "1" ]; then
-	proj=$1
+	proj_name=$1
 else
-	proj=$(basename $(pwd))
+	proj_name=$(basename $(pwd))
 fi
 
-echo "Creating project $proj"
+if [ -e "$SLICKEDITPROJ" ]; then
+	proj_file=$SLICKEDITPROJ/$proj_name
+else
+	proj_file=$(pwd)/$proj_name
+fi
+
+echo "Creating workspace $proj_file.vpw"
 
 exe=$0
 dir=$(dirname $exe)
-cp -f $dir/template.vpw ./$proj.1.vpw
-cp -f $dir/template.vpj ./$proj.1.vpj
-sed "s/%PROJECTNAME/$proj/" $proj.1.vpw > $proj.vpw
-sed "s/%PROJECTNAME/$proj/" $proj.1.vpj > $proj.2.vpj
+cp -f $dir/template.vpw $proj_file.1.vpw
+cp -f $dir/template.vpj $proj_file.1.vpj
+sed "s/%PROJECTNAME/$proj_name/" $proj_file.1.vpw > $proj_file.vpw
+sed "s/%PROJECTNAME/$proj_name/" $proj_file.1.vpj > $proj_file.2.vpj
 
 find . -name "*.cpp" -o -name "*.c" -o -name "*.h" -o -name "*.cc" -o -name "*.hxx" -o -name "*.hpp" -o -name "*.sh" -o -name "*.py" -o -name "*.pl" \
--o -name "Makefile" -o -name "*.def" -o -name "README.*" -o -name "README" | grep -v "\.svn" | sed 's/^\.\///' | awk '{ print "<F N=\""$1"\"/>" }' > $proj.files
-sed -e "/%PROJECTFILES/r $proj.files" -e '/%PROJECTFILES/d' $proj.2.vpj > $proj.vpj
+-o -name "Makefile" -o -name "*.make" -o -name "*.mak" -o -name "*.def" -o -name "README.*" -o -name "README" | grep -v "\.svn" | sed 's/^\.\///' > $proj_file.files1
 
-rm -f $proj.1.vpw
-rm -f $proj.1.vpj
-rm -f $proj.2.vpj
-rm -f $proj.files
+for file in $(cat $proj_file.files1); do
+	if [ ! -h $file ]; then
+		file2=$(realpath $file)
+		echo $file2 >> $proj_file.files2
+	fi
+done
+
+cat $proj_file.files2 | awk '{ print "<F N=\""$1"\"/>" }' > $proj_file.files
+
+sed -e "/%PROJECTFILES/r $proj_file.files" -e '/%PROJECTFILES/d' $proj_file.2.vpj > $proj_file.vpj
+
+rm -f $proj_file.1.vpw
+rm -f $proj_file.1.vpj
+rm -f $proj_file.2.vpj
+rm -f $proj_file.files
+rm -f $proj_file.files1
+rm -f $proj_file.files2
